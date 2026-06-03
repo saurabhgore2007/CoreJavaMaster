537a657d62aaa56be2ed87cc8c34e41c1a376cf6
saurabhgore2007 <saurabhgore44@gmail.com>
2026-06-03 13:07:10 +0530

    Program forInput a number and display whether it is Odd or Even

---
+++ src/Day2/Program3.java
@@ -0,0 +1,24 @@
+package Day2;
+
+import java.util.Scanner;
+
+// Input a  number and display whether it is Odd or Even  
+public class Program3 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);	
+				
+		System.out.print("Enter a Number :- ");
+		int num = scan.nextInt();
+		
+		
+		if(num % 2 == 0) 
+			System.out.println(num + " is Even");
+		else 
+			System.out.println(num + " is Odd");
+		
+		
+		scan.close();
+	}
+}
