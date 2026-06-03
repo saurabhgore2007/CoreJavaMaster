a3ee4fc5e86549e747d39cdb960b3c8ae22f1ae2
saurabhgore2007 <saurabhgore44@gmail.com>
2026-06-03 13:28:44 +0530

    Program to calculate factorial using while Loop

---
+++ src/Day2/Program6.java
@@ -0,0 +1,30 @@
+package Day2;
+
+import java.util.Scanner;
+
+// Program to calculate factorial using while.
+
+public class Program6 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		double num,fact = 1 ,i=1;
+		
+        System.out.print("Enter a non-negative integer: ");
+		num = scan.nextInt();
+		
+		if(num < 0) {
+			System.out.println("Invalid Input! Please Enter a Number.");
+			return;
+		}
+		while(i<=num) {
+			fact *= i;
+			i++;
+		}
+		
+		System.out.print("Factorial of " + num + " is " + fact);
+		scan.close();
+	}
+}
