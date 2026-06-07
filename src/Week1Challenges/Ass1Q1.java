4071bafc17f8f58473e2f0069661d6e618a3d975
saurabhgore2007 <saurabhgore44@gmail.com>
2026-06-07 19:17:19 +0530

    Week 1 Challenge 1 Task 1 Completed

---
+++ src/Week1Challenges/Ass1Q1.java
@@ -0,0 +1,29 @@
+package Week1Challenges;
+
+import java.util.Scanner;
+
+/*WAP to input width and height of a rectangle and calculate the area and perimeter.  
+[Area of rectangle: width*height, Perimeter of rectangle: 2*width+2*height]*/
+
+public class Ass1Q1 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		float width,height,area,perimeter;
+		
+		System.out.print("Enter Width :- ");
+		width = scan.nextFloat();
+		
+		System.out.print("Enter Height :- ");
+		height = scan.nextFloat();
+		
+		area = width*height;
+		perimeter = 2*width+2*height;
+
+		System.out.println("Enter Area Of Rectangle :- " + area);
+		System.out.println("Enter Primeter Area Of Rectangle :- " + perimeter);
+	}
+
+}
